0f2baaab49a39aec74d92270cdbd3ace0fb4329a
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-20 19:04:16 -0400

    comment

--- script.js
+++ script.js
@@ -127,23 +127,4 @@ class Calculator {
  
 
 
-// let expression = '';
-
-// // press get num plus that expression by its id give me the valu wh
-// // which gets me that expression 
-// function press(num){
-// expression += num;
-// document.getElementById("entry").value = expression;
-// }
-
-// function equal(){
-//    document.getElementById("entry").value = eval(expression); 
-//    expression = '';
-// }
-
-// function erase(){
-//     expression = '';
-//     document.getElementById("entry").value = expression;
-
-//     }
-
+//  these buttons ??? the operations buttons are not working ?
