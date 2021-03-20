81cb2ff9357b9ac7a3e9866469f16199a16c0abd
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-20 14:34:22 -0400

    js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let expression = '';
+// let expression = '';
 
-// press get num plus that expression by its id give me the valu wh
-// which gets me that expression 
-function press(num){
-expression += num;
-document.getElementById("entry").value = expression;
-}
+// // press get num plus that expression by its id give me the valu wh
+// // which gets me that expression 
+// function press(num){
+// expression += num;
+// document.getElementById("entry").value = expression;
+// }
 
-function equal(){
-   document.getElementById("entry").value = eval(expression); 
-   expression = '';
-}
+// function equal(){
+//    document.getElementById("entry").value = eval(expression); 
+//    expression = '';
+// }
 
-function erase(){
-    expression = '';
-    document.getElementById("entry").value = expression;
+// function erase(){
+//     expression = '';
+//     document.getElementById("entry").value = expression;
 
-    }
+//     }
 
