2afcb1d635f17e85b37cf2f6c047347f4154b792
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-20 14:41:41 -0400

    js const

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousButtonTextElement = document.querySelector('[data-previous-operand]')
+const currentButtonTextElement = document.querySelector('[data-current-operand]')
+
+
+
+
+
+
 // let expression = '';
 
 // // press get num plus that expression by its id give me the valu wh
